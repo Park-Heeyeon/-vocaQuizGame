e7bd2a15384674a8033a616654ca6e0cec7fc043
heeyeon <heeyeon8662@gmail.com>
2024-10-19 01:36:20 +0900

    feat: shadcn/ui 설치 및 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/**/*.{js,jsx,ts,tsx}", // 기존 경로
     "./node_modules/shadcn-ui/**/*.{js,jsx,ts,tsx}", // shadcn/ui 경로 추가
   ],
   theme: {
-    extend: {
-      colors: {
-        customBlueColor: "#61acc5", // 원하는 색상 추가
-        customGrayColor: "#babbbb",
-      },
-    },
+  	extend: {
+  		colors: {
+  			customBlueColor: '#61acc5',
+  			customGrayColor: '#babbbb'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // alias 설정: @를 사용해 src 디렉터리의 파일들을 경로에서 간단히 가져올 수 있습니다.
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
